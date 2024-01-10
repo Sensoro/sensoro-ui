912725388388196e60ed9f027e8aed08b27c7edc
wangxingkang <wang_xingkang@qq.com>
2024-01-10 12:59:09 +0800

    chore: deploy test

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,19 +3,22 @@ import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { visualizer } from 'rollup-plugin-visualizer'
 
-export default defineConfig({
-  plugins: [
-    sveltekit(),
-    visualizer({
-      emitFile: true,
-      filename: 'stats.html',
-      template: 'treemap',
-      brotliSize: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      $components: path.resolve('/src/components'),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? 'sensoro-ui' : '',
+    plugins: [
+      sveltekit(),
+      visualizer({
+        emitFile: true,
+        filename: 'stats.html',
+        template: 'treemap',
+        brotliSize: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        $components: path.resolve('/src/components'),
+      },
     },
-  },
+  }
 })
